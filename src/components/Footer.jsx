11cae1d59b9853c27883003ff3d200b9ac65b39f
bengin34 <becaglar3434@gmail.com>
2023-04-04 17:57:12 +0300

    detail

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,10 @@ import { Box, Stack, Typography } from '@mui/material'
 
 const Footer = () => {
   return (
-    <Box mt="40px" bgcolor="#1B76D2" >
+    <Box mt="40px"  >
             <Stack gap="40px" alignItems="center" px="40px"  >
-              <Typography variant='h6' pb="4px" mt="10px" >
-                Made with  ❤️
+              <Typography variant='p' pb="4px" mt="10px" >
+                Made with  ❤️ 
               </Typography>
             </Stack>
     </Box>
--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -7,12 +7,15 @@ import useBlogCall from "../hooks/useBlogCall";
 import { useSelector } from "react-redux";
 import Typography from "@mui/material/Typography";
 import { Grid } from "@mui/material";
+import Button from "@mui/material/Button";
+import Paper from "@mui/material/Paper";
+import Avatar from "@mui/material/Avatar";
+
 const Detail = () => {
   const [blogDetails, setBlogDetails] = useState("");
   const { blogs } = useSelector((state) => state.blog);
   const { id } = useParams();
   const { readBlogData } = useBlogCall();
-  console.log(id);
 
   useEffect(() => {
     readBlogData("blogs", id);
@@ -23,7 +26,7 @@ const Detail = () => {
     <Container>
       <Box>
         <Box padding={5} sx={{ display: "flex", justifyContent: "center" }}>
-          <img width="50%" height="50%" src={blogs?.image} />
+          <img width="50%" src={blogs?.image} />
         </Box>
         <Container>
           <Box
@@ -35,24 +38,60 @@ const Detail = () => {
               minHeight: "300px",
             }}
           >
-            <Typography variant="h3" sx={{ display: "block" }}>
-              {blogs?.title}
-            </Typography>
-            <Typography marginTop={2} variant="h6">
-              {blogs?.content}
-            </Typography>
-            <Typography marginTop={2} variant="p">
-              Written by: {blogs?.author} Created Date:{" "}
-              {blogs?.publish_date?.slice(0, 10)}{" "}
-            </Typography>
-            <Typography marginTop={2} variant="h6">
-              Comments
-            </Typography>
-            {blogs?.comments?.map((item, index) => (
-              <Grid key={index}>
-                <Typography key={index}>{item}</Typography>
-              </Grid>
-            ))}
+            <Paper  sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                flexWrap: "wrap",
+                gap: "1rem",
+                padding: "3rem",
+              }}>
+              {" "}
+              <Typography variant="h3" sx={{ display: "block" }}>
+                {blogs?.title}
+              </Typography>
+              <Typography marginTop={2} variant="h6">
+                {blogs?.content}
+              </Typography>
+              <Typography marginTop={2} variant="p" marginBottom={5}>
+                Written by: {blogs?.author} Created Date:{" "}
+                {blogs?.publish_date?.slice(0, 10)}{" "}
+              </Typography>{" "}
+            </Paper>
+
+            <Paper
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                flexWrap: "wrap",
+                gap: "1rem",
+                margin: "10px",
+              }}
+            >
+              {blogs?.comments?.map((item, index) => (
+                <Paper key={index} style={{ padding: "40px 20px", margin: 10 }}>
+                  <Grid container wrap="nowrap" spacing={2}>
+                    <Grid item>
+                      <Avatar alt="Remy Sharp" src={blogs?.image} />
+                    </Grid>
+                    <Grid justifyContent="left" item xs zeroMinWidth>
+                      <h4 style={{ margin: 0, textAlign: "left" }}>
+                        {item?.user}
+                      </h4>
+                      <p style={{ textAlign: "left" }}>{item?.content} </p>
+                      <p style={{ textAlign: "left", color: "gray" }}>
+                        posted {item?.time_stamp.slice(11, 16)} /{" "}
+                        {item?.time_stamp.slice(0, 10)}
+                      </p>
+                    </Grid>
+                  </Grid>
+                </Paper>
+              ))}
+            </Paper>
+            <Button sx={{ marginTop: "20px" }} variant="outlined">
+              Add New Post
+            </Button>
           </Box>
         </Container>
       </Box>
